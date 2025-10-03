f7ac64c33b0c8eafdbe37b6b7a162bc42bb2ff4d
Prathima Uggappakodi <prathima.uggappakodi@alifsemi.com>
2025-10-03 13:44:54 -0700

    Committing the sensor sampling demo c file for E8 changes

--- LowPowerSensorSampling/sensor_sampling_demo.c
+++ LowPowerSensorSampling/sensor_sampling_demo.c
@@ -71,16 +71,22 @@ static void configure_spi_pinmux()
             PADCTRL_SCHMITT_TRIGGER_ENABLE |
             PADCTRL_OUTPUT_DRIVE_STRENGTH_4MA;
 
+    #if defined(ENSEMBLE_SOC_GEN2)
+             /* Port 1 */
+     HW_REG32(PINMUX_BASE, 0x20) = 2 | (padconf << 16);  /* P1_0 is SPI0_MISO */
+     HW_REG32(PINMUX_BASE, 0x24) = 2 | (padconf << 16);  /* P1_1 is SPI0_MOSI */
+     HW_REG32(PINMUX_BASE, 0x28) = 2 | (padconf << 16);  /* P1_2 is SPI0_CLK */
+     HW_REG32(PINMUX_BASE, 0x2C) = 2 | (padconf << 16);  /* P1_3 is SPI0_SS0 */
+
+     #else
+
     /* Port 5 */
      HW_REG32(PINMUX_BASE, 0xA0) = 4 | (padconf << 16);  /* P5_0 is SPI0_MISO */
      HW_REG32(PINMUX_BASE, 0xA4) = 4 | (padconf << 16);  /* P5_1 is SPI0_MOSI */
      HW_REG32(PINMUX_BASE, 0xA8) = 4 | (padconf << 16);  /* P5_2 is SPI0_SS0 */
      HW_REG32(PINMUX_BASE, 0xAC) = 3 | (padconf << 16);  /* P5_3 is SPI0_SCLK */
 
-   /* HW_REG32(PINMUX_BASE, 0xA0) = 2 | (padconf << 16);  /* P5_0 is SPI0_MISO */
-   // HW_REG32(PINMUX_BASE, 0xA4) = 2 | (padconf << 16);  /* P5_1 is SPI0_MOSI */
-    //HW_REG32(PINMUX_BASE, 0xA8) = 2 | (padconf << 16);  /* P5_2 is SPI0_SS0 */
-    //HW_REG32(PINMUX_BASE, 0xAC) = 2 | (padconf << 16);  /* P5_3 is SPI0_SCLK */
+     #endif
 
     /* DEBUG */
     HW_REG32(PINMUX_BASE, 0xB0) = padconf << 16; /* P5_4 is GPIO5_4 */
@@ -217,7 +223,6 @@ void low_power_sensor_sampling_demo()
 
         /* switch clocks to HFXO */
         HW_REG32(GPIO5_BASE,  0x00) = 1U << 4;  // clock config start
-
         /* set the clock variables */
         SystemAXIClock = (HFXO_CLK);          // SYST_ACLK is SYSPLL div by 4
         SystemAHBClock = (HFXO_CLK);          // SYST_HCLK is ACLK div by 1
